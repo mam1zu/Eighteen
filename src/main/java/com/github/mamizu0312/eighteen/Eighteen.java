366b6790179a988ea61d42722d4001e89dffa1ac
BuildTools <unconfigured@null.spigotmc.org>
2018-12-17 00:29:20 +0900

    v0.2.9b 状況確認用2

--- src/main/java/com/github/mamizu0312/eighteen/Eighteen.java
+++ src/main/java/com/github/mamizu0312/eighteen/Eighteen.java
@@ -3,11 +3,12 @@ package com.github.mamizu0312.eighteen;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Eighteen extends JavaPlugin{
+    EighteenEvent event = new EighteenEvent();
     @Override
     public void onEnable() {
         getLogger().info("Enabled.");
         getCommand("eighteen").setExecutor(new EighteenCommandManager());
-        getServer().getPluginManager().registerEvents(new EighteenEvent(), hogehoge);
+        getServer().getPluginManager().registerEvents(event, this);
     }
 
     @Override
